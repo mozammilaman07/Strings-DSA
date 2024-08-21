5c1c2874b3786f088fd15139ddd143387c037171
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-21 23:08:49 +0530

    used erase function

--- string.cpp
+++ string.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 int main()
 {
-    string name;
-    getline(cin, name);
-    // empty string
-    string temp = "";
-    // cout << "Length of string " << name.length() << endl;
-    // cout << "string is empty or not " << temp.empty() << endl;
+    //     string name;
+    //     getline(cin, name);
+    //     // empty string
+    //     string temp = "";
+    //     // cout << "Length of string " << name.length() << endl;
+    //     // cout << "string is empty or not " << temp.empty() << endl;
 
-    // cout << "Character at index 1 is " << name.at(1) << endl;
-    string str1 = "Love";
-    string str2 = "Babbar";
-    cout << "Before " << str1 << endl;
-    cout << "Before " << str2 << endl;
-    // apend
-    str1.append(str2);
-    cout << "After " << str1 << endl;
-    cout << "After " << str2 << endl;
+    //     // cout << "Character at index 1 is " << name.at(1) << endl;
+    //     string str1 = "Love";
+    //     string str2 = "Babbar";
+    //     cout << "Before " << str1 << endl;
+    //     cout << "Before " << str2 << endl;
+    //     // apend
+    //     str1.append(str2);
+    //     cout << "After " << str1 << endl;
+    //     cout << "After " << str2 << endl;
+    string desc = "This is a car";
+    desc.erase(4, 3);
+    cout << "to erase character " << desc << endl;
 }
