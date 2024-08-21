ff6ba8665f94ab0f01d21504ef1b3b01b8d67376
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-21 22:37:28 +0530

    used empty function

--- string.cpp
+++ string.cpp
@@ -6,6 +6,8 @@ int main()
 {
     string name;
     getline(cin, name);
+    // empty string
+    string temp = "";
     cout << "Length of string " << name.length() << endl;
-    ;
+    cout << "string is empty or not " << temp.empty() << endl;
 }
