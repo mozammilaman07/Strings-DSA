e392ad573bbaf8705f5d0e998601b3e75c48c03c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-21 22:47:31 +0530

    used append function

--- string.cpp
+++ string.cpp
@@ -11,5 +11,13 @@ int main()
     // cout << "Length of string " << name.length() << endl;
     // cout << "string is empty or not " << temp.empty() << endl;
 
-    cout << "Character at index 1 is " << name.at(1) << endl;
+    // cout << "Character at index 1 is " << name.at(1) << endl;
+    string str1 = "Love";
+    string str2 = "Babbar";
+    cout << "Before " << str1 << endl;
+    cout << "Before " << str2 << endl;
+    // apend
+    str1.append(str2);
+    cout << "After " << str1 << endl;
+    cout << "After " << str2 << endl;
 }
