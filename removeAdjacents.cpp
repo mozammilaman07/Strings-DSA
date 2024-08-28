176e332d18df446c6d58f399c6c0227bad2cc1d9
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-28 12:36:26 +0530

    created main function and remove Deplicate function

---
+++ removeAdjacents.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+string removeDuplicates(string s)
+{
+}
+int main()
+{
+
+    string s = "abbaca";
+
+    removeDuplicates(s);
+    cout << s << endl;
+}
