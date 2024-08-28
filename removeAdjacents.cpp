a1afc10f3e5f5899ebbb4f2e6891b0b836e3e75b
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-28 12:44:42 +0530

    code done

--- removeAdjacents.cpp
+++ removeAdjacents.cpp
@@ -4,12 +4,27 @@ using namespace std;
 
 string removeDuplicates(string s)
 {
+    string ans = "";
+    int index = 0;
+    while (index < s.length())
+    {
+        if (ans.length() > 0 && ans[ans.length() - 1] == s[index])
+        {
+            ans.pop_back();
+        }
+        else
+        {
+            ans.push_back(s[index]);
+        }
+        index++;
+    }
+    return ans;
 }
 int main()
 {
 
     string s = "abbaca";
 
-    removeDuplicates(s);
-    cout << s << endl;
+    string sa = removeDuplicates(s);
+    cout << sa << endl;
 }
